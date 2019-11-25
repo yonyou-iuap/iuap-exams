274aa801e7a52138f2534a725ae1619f09646e2b
gaoshja <gaoshja@yonyou.com>
2019-11-25 13:44:34 +0800

    iuap考试项目

--- src/main/java/com/yonyou/iuap/exams/web/HelloController.java
+++ src/main/java/com/yonyou/iuap/exams/web/HelloController.java
@@ -1,12 +1,15 @@
 package com.yonyou.iuap.exams.web;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    @Value("${info}")
+    private String info="Welcome to iuap5.0 world!";
     @GetMapping("/")
     public String hello(){
-        return "Welcome to iuap5.0 world!";
+        return info;
     }
 }
