b94767a2bcc71b1b93429bb2d9ffd6b8854b08d8
gaoshja <gaoshja@yonyou.com>
2019-12-30 20:20:57 +0800

    iuap考试项目-myds

--- src/main/java/com/yonyou/iuap/exams/web/WelcomeController.java
+++ src/main/java/com/yonyou/iuap/exams/web/WelcomeController.java
@@ -1,17 +1,21 @@
 package com.yonyou.iuap.exams.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WelcomeController {
+    Logger logger = LoggerFactory.getLogger("iuap-exams");
     @GetMapping("/welcome")
     public String welcome(){
         return "welcome";
     }
 
-    @GetMapping("/welcomejsp")
+    @GetMapping("/jsp")
     public String welcomeJsp(){
-        return "welcome.jsp";
+        logger.debug("welcome jsp.");
+        return "test";
     }
 }
